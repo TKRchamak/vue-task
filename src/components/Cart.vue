5948704e84b04acaf6c0f9a67e63de6e21b5cf98
Tonmoy kumar Roy <tonmoykumarroy6@gmail.com>
2021-10-10 22:22:39 +0600

    add bye functionality

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-8 border-end">
+        <!-- <div v-for="pd in $store.state.cartData" :key="pd.id" class="card mb-3"> -->
         <div v-for="pd in cartData" :key="pd.id" class="card mb-3">
           <div class="row g-0">
             <div class="col-md-4">
@@ -11,10 +12,12 @@
               <div class="card-body">
                 <div class="d-flex justify-content-between">
                   <h5 class="card-title">Selected Phone</h5>
-                  <button @click="removeData(pd.id)" class="btn btn-danger">Delete</button>
+                  <button @click="removeData(pd.id)" class="btn btn-danger">
+                    Delete
+                  </button>
                 </div>
-                <p class="card-text">Price : {{pd.charge}}</p>
-                <h4 class="card-text">Quantity : {{pd.quantity}}</h4>
+                <p class="card-text">Price : {{ pd.charge }}</p>
+                <h4 class="card-text">Quantity : {{ pd.quantity }}</h4>
               </div>
             </div>
           </div>
@@ -43,25 +46,26 @@
 </template>
 
 <script>
+import { mapMutations, mapState } from "vuex";
+
 export default {
   name: "Cart",
 
   data() {
     return {
-      cartData: [],
       totalCost: 0,
       totalQuantity: 0,
     };
   },
   created() {
     let data = this.getData();
+    this.$store.state.cartData = data; //check leter
     let cartSelectedData = Object.values(data);
-    this.cartData = data;
-    console.log(data);
     this.totalPrice(cartSelectedData);
     this.totalProductQuantity(cartSelectedData);
   },
   methods: {
+    ...mapMutations(["processOrder"]),
     getData() {
       const data = localStorage.getItem("user") || "{}";
       return JSON.parse(data);
@@ -70,9 +74,6 @@ export default {
       const currentCart = this.getData();
       delete currentCart[key];
       localStorage.setItem("user", JSON.stringify(currentCart));
-    },
-    processOrder() {
-      localStorage.removeItem(this.getData());
     },
     totalPrice(value) {
       const total = value.reduce(
@@ -85,6 +86,11 @@ export default {
       const total = value.reduce((sum, event) => sum + event.quantity, 0);
       this.totalQuantity = total;
     },
+  },
+
+  //data call from store
+  computed: {
+    ...mapState(["cartData"]),
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        cartData: [],
+        totalCost: 0,
+        totalQuantity: 0,
+    },
+    getters: {
+
+    },
+    mutations: {
+        totalPrice(state, value) {
+            state.totalCost = value.reduce((sum, event) => sum + event.charge * event.quantity, 0)
+        },
+        processOrder(state) {
+            localStorage.removeItem("user");
+        },
+    },
+    actions: {},
+    modules: {}
 })
